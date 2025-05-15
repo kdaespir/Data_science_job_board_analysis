b35397c9590f305c1dfa3911aaf6bba3ecea445e
kdaespir <krisespiritu5@gmail.com>
2025-05-15 15:55:06 -0400

    Update linkedin_data_jobs_analysis.py

--- linkedin_data_jobs_analysis.py
+++ linkedin_data_jobs_analysis.py
@@ -6,30 +6,32 @@ data = pd.read_csv('clean_jobs.csv')
 # Gives the number of null columns in each feature
 # print(data.isnull().sum())
 
-companies_breakdown = duckdb.sql("""
-            select company, count(id) as counts
-            from data
-            group by company
-            order by counts desc, company desc
-               """)
+def jobs_by_company(num_companies=4):
+   companies_breakdown = duckdb.sql("""
+               select company, count(id) as counts
+               from data
+               group by company
+               order by counts desc, company desc
+                  """)
 
-companies_w_most_jobs = duckdb.sql('''
-               with cte as (
-               select *, 
-               from companies_breakdown
-               limit 4)
+   companies_w_most_jobs = duckdb.sql(f'''
+                  with cte as (
+                  select *, 
+                  from companies_breakdown
+                  limit {num_companies})
 
-               select *
-               from cte
+                  select *
+                  from cte
 
-               union 
+                  union 
 
-               select 'Other' as company, sum(counts) as count
-               from companies_breakdown 
-               where company not in (select company from cte)
+                  select 'Other' as company, cast(sum(counts) as INTEGER) as count
+                  from companies_breakdown 
+                  where company not in (select company from cte)
 
-               order by company
-               ''').df()
+                  order by company
+                  ''').df()
+   return companies_w_most_jobs
                
 
-print(_)
+print(data['title'].unique())
